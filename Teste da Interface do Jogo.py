567be4aa91e3ebd44b70f4898cf692b4e5217097
Riuk2252 <100776687+Riuk2252@users.noreply.github.com>
2022-11-14 09:22:59 -0300

    Add files via upload

---
+++ Teste da Interface do Jogo.py
@@ -0,0 +1,549 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+def menuiniciar1 (): #opção em português do menu inicial do jogo
+    print('╔╦═╦╦══╦══╦═╦╦═╦╦══╦╦═╦══╦══╦══╦╗')
+    print('║║║║║╔╗║║║║═╣║║╚╣╔╗║╚╗║╔╗║╔═╣╔╗║║')
+    print('║║║║║╚╝║║║║═╣║║╔╣╚╝║╔╣║╚╝║╚╝║╚╝║║')
+    print('║╚╩═╩══╩╩╩╩═╝╚═╝╚══╝╚═╩══╩══╩══╝║')
+    print('╚═══════════════════════════════╝')
+
+    print('\t   ╔╦═╦═╦═╦╦╗')
+    print('\t  ╔╝║║║╔╣╩║╔╝')
+    print('\t  ╚═╩═╩═╩╩╩╝')
+
+    print('        ╔═╦═╦═╦═╦═╦══╗')
+    print('        ║║║╔╣╠╣║║╦╬╗╚╣')
+    print('        ╚═╩╝╚═╩═╩═╩══╝')
+
+    print('\t   ╔╗╔═╗╔╦═╗')
+    print('\t   ║╚╣║╠╝║╩║')
+    print('\t   ╚═╩═╩═╩╩╝')
+
+    print('\t   ╔══╦═╦╦╦╗')
+    print('\t   ╠╗╚╣╩║║╔╝')
+    print('\t   ╚══╩╩╩╩╝')
+        
+def menuiniciar2 (): #opção em inglês do menu inicial do jogo
+    print('╔╦═╦╦══╦══╦═╦╦═╦╦══╦╦═╦══╦══╦══╦╗')
+    print('║║║║║╔╗║║║║═╣║║╚╣╔╗║╚╗║╔╗║╔═╣╔╗║║')
+    print('║║║║║╚╝║║║║═╣║║╔╣╚╝║╔╣║╚╝║╚╝║╚╝║║')
+    print('║╚╩═╩══╩╩╩╩═╝╚═╝╚══╝╚═╩══╩══╩══╝║')
+    print('╚═══════════════════════════════╝')
+
+    print('\t   ╔═╦╗╔═╦╦╗')
+    print('\t   ║╔╣╚╣╩╠╗║')
+    print('\t   ╚╝╚═╩╩╝╚╝')
+
+    print('       ╔═╦═╦══╦╦═╦═╦╦══╗')
+    print('       ║║║╔╩╗╔╣║║║║║╠╗╚╣')
+    print('       ╚═╩╝ ╚╝╚╩═╩╩═╩══╝')
+
+    print('\t   ╔══╦╦╦═╦═╗')
+    print('\t   ╠╗╚╣═║║║╔╝')
+    print('\t   ╚══╩╩╩═╩╝')
+
+    print('\t   ╔═╦╦╦╦══╗')
+    print('\t   ║╦╬ ╣╠╗╔╝')
+    print('\t   ╚═╩╩╩╝╚╝')
+
+def opcoes1 (): #se o usuário quiser configurar o jogo ou ler as regras (em português)
+    print('\t╔╦══╦═╦═╦══╦═╦═╦╗')
+    print('\t║║╔╗║░║╔╣╔╗║═╣╚╣║')
+    print('\t║║╚╝║╔╣╚╣╚╝║═╬╗║║')
+    print('\t║╚══╩╝╚═╩══╩═╩═╝║')
+    print('\t╚═══════════════╝')
+    
+    print('\n\t ╔╦╦═╦═╦╦╦═╦══╗')
+    print('\t ║╔╣╦╣╔╣╔╣╩╠╗╚╣')
+    print('\t ╚╝╚═╩═╩╝╚╩╩══╝')
+    
+    print('     ╔═╦╦═╦╦═╦╗╔╦╗╔═╦═╦═╦═╗')
+    print('     ║╠║║╔╣║╠╣╚╝║╚╣╠║╩║╠║╦╣')
+    print('     ╚═╩╩╝╚╩═╩══╩═╩═╩╩╩═╩═╝')
+    
+    print('\t  ╔╦═╦╦═╦══╦═╗')
+    print('\t  ║║╠║║║║║║║╩║')
+    print('\t  ╚╩═╩╩═╩╩╩╩╩╝')
+    
+    print('\t  ╔══╦═╦═╦╦╗╔╗')
+    print('\t  ║║║║╦╣║║║╚╝║')
+    print('\t  ╚╩╩╩═╩╩═╩══╝')
+    
+def opcoes2 (): #se o usuário quiser configurar o jogo ou ler as regras (em inglês)
+    print('\t╔╦══╦═╦══╦╦══╦═╦╦═╦╗')
+    print('\t║║╔╗║░╠╣╠╣║╔╗║║║║╚╣║')
+    print('\t║║╚╝║╔╝║║║║╚╝║║║╠╗║║')
+    print('\t║╚══╩╝ ╚╝╚╩══╩╩═╩═╝║')
+    print('\t╚══════════════════╝')
+    
+    print('\n\t   ╔╦╦╗╔╦╗╔═╦══╗')
+    print('\t   ║╔╣╚╝║╚╣╦╬╗╚╣')
+    print('\t   ╚╝╚══╩═╩═╩══╝')
+    
+    print('       ╔═╦╦═╦═╦╦═╦╗╔╦╗╔══╦╦╗')
+    print('       ║╠║║╔╣╔╣║╠╣╚╝║╚╬╗╔╩╗║')
+    print('       ╚═╩╩╝╚╝╚╩═╩══╩═╝╚╝ ╚╝')
+    
+    print('\t╔╗╔═╦═╦╦═╦╗╔╦═╦═╦═╗')
+    print('\t║╚╣╩║║║║╔╣╚╝║╩║╔╣╦╣')
+    print('\t╚═╩╩╩╩═╩═╩══╩╩╩═╩═╝')
+    
+    print('\t   ╔══╦═╦═╦╦╗╔╗')
+    print('\t   ║║║║╦╣║║║╚╝║')
+    print('\t   ╚╩╩╩═╩╩═╩══╝')
+    
+#opção de regras em opções
+def regras_escrita1(): #regras do jogo escritas em português
+    print('\n1: ')
+    
+def regras_escrita2(): #regras do jogo escritas em inglês
+    print('\n1: ')
+
+def regras1 (): #função de regras caso o usuário escolha ler as regras do jogo (português)
+    print('\t╔╦══╦═╦══╦══╦═╦═╦╗')
+    print('\t║║░░║═╣╔═╣░░║║║╚╣║')
+    print('\t║║╔╗╣═╣╚╝║╔╗╣╩╠╗║║')
+    print('\t║╚╝╚╩═╩══╩╝╚╩╩╩═╝║')
+    print('\t╚════════════════╝')
+    
+    regras_escrita1()
+    
+    print('\n\t  ╔╦╦═╦╗╔══╦═╦╦╗')
+    print('\t  ║║║║║╚╬╗╔╣╩║╔╝')
+    print('\t  ╚═╩═╩═╝╚╝╚╩╩╝')
+    
+def regras2 (): #função de regras caso o usuário escolha ler as regras do jogo (inglês)
+    print('\t╔╦══╦╗╔╦╦╦═╦═╦╗')
+    print('\t║║░░║║║║║║═╣╚╣║')
+    print('\t║║╔╗╣╚╝║╚╣═╬╗║║')
+    print('\t║╚╝╚╩══╩═╩═╩═╝║')
+    print('\t╚═════════════╝')
+    
+    regras_escrita2()
+    
+    print('\n\t   ╔═╦═╦═╦╦╗')
+    print('\t   ║╣╣╩║╠╣═╣')
+    print('\t   ╚═╩╩╩═╩╩╝')
+    
+#opção de dificuldade em opções
+def dificuldade1 (): #se o usuário quiser alterar a dificuldade do jogo (em português)
+    print('\t╔╦═╦╦╦═╦╦═╦╗╔╦╦╦═╦╦═╦═╦╦═╦╗')
+    print('\t║║║╚╣║═╣║╔╣║║║║║║╚╣║║║╚╣═╣║')
+    print('\t║║║╔╣║╔╣║╚╣╚╝║╚╣║╔╣╩║║╔╣═╣║')
+    print('\t║╚═╝╚╩╝╚╩═╩══╩═╩═╝╚╩╩═╝╚═╝║')
+    print('\t╚═════════════════════════╝')
+    
+    print('\n\t\t ╔═╦═╦═╦╦╗')
+    print('\t\t ║╔╣╩║╠╣║╚╗')
+    print('\t\t ╚╝╚╩╩═╩╩═╝')
+    
+    print('\t\t╔══╦═╦═╦╦═╗')
+    print('\t\t║║║║╦╣╠║║║║')
+    print('\t\t╚╩╩╩═╩═╩╩═╝')
+    
+    print('\t\t╔═╦╦═╦╦═╦╦╗')
+    print('\t\t║╠║║╔╣║╠╣║╚╗')
+    print('\t\t╚═╩╩╝╚╩═╩╩═╝')
+    
+    print('\t       ╔╦╦═╦╗╔══╦═╦╦╗')
+    print('\t       ║║║║║╚╬╗╔╣╩║╔╝')
+    print('\t       ╚═╩═╩═╝╚╝╚╩╩╝')
+    
+def dificuldade2 (): #se o usuário quiser alterar a dificuldade do jogo (em inglês)
+    print('\t╔╦═╦╦╦═╦═╦╦═╦╗╔╦╦╦══╦═╦╦╗')
+    print('\t║║║╚╣║═╣═╣║╔╣║║║║╚╣╠╣ ║║║')
+    print('\t║║║╔╣║╔╣╔╣║╚╣╚╝║╚╗║║╚╗╔╝║')
+    print('\t║╚═╝╚╩╝╚╝╚╩═╩══╩═╝╚╝ ╚╝ ║')
+    print('\t╚═══════════════════════╝')
+    
+    print('\n\t       ╔═╦═╦══╦╦╗')
+    print('\t       ║╦╣╩╠╗╚╬╗║')
+    print('\t       ╚═╩╩╩══╝╚╝')
+    
+    print('\t     ╔══╦═╦═╦╦╗╔╦══╗')
+    print('\t     ║║║║╦╣╠║║╚╝║║║║')
+    print('\t     ╚╩╩╩═╩═╩╩══╩╩╩╝')
+    
+    print('\t\t╔╦╦═╦╦╦═╗')
+    print('\t\t║═║╩║╔╣╠║')
+    print('\t\t╚╩╩╩╩╝╚═╝')
+    
+    print('\t\t╔═╦═╦═╦╦╗')
+    print('\t\t║╣╣╩║╠╣═╣')
+    print('\t\t╚═╩╩╩═╩╩╝')
+
+#apção de idiomas em opções
+def idioma1 (): #se o usuário quiser alterar o idioma (em português)
+    print('\t╔╦╦═╦╦╦══╦══╦═╦╗')
+    print('\t║║║║╚╣║╔╗║║║║║║║')
+    print('\t║║║║╔╣║╚╝║║║║╩║║')
+    print('\t║╚╩═╝╚╩══╩╩╩╩╩╝║')
+    print('\t╚══════════════╝')
+    
+    print('\n    ╔═╦═╦╦╦══╦╗╔╦═╦╗╔╦═╦══╗')
+    print('    ║╔╣║║╔╩╗╔╣╚╝║╔╣╚╝║╦╬╗╚╣')
+    print('    ╚╝╚═╩╝ ╚╝╚══╩═╩══╩═╩══╝')
+    
+    print('\t ╔╦═╦╦═╦╗╔═╦══╗')
+    print('\t ║║║║║╔╣╚╣╦╬╗╚╣')
+    print('\t ╚╩╩═╩═╩═╩═╩══╝')
+    
+    print('\t ╔╦╦═╦╗╔══╦═╦╦╗')
+    print('\t ║║║║║╚╬╗╔╣╩║╔╝')
+    print('\t ╚═╩═╩═╝╚╝╚╩╩╝')
+
+def idioma2 (): #se o usuário quiser alterar o idioma (em inglês)
+    print('\t╔╦╦╦═╦═╦╦══╦╗╔╦═╦══╦═╦╗')
+    print('\t║║║║║║║║║╔═╣║║║║║╔═╣═╣║')
+    print('\t║║╚╣╩║║║║╚╝║╚╝║╩║╚╝║═╣║')
+    print('\t║╚═╩╩╩╩═╩══╩══╩╩╩══╩═╝║')
+    print('\t╚═════════════════════╝')
+    
+    print('\n       ╔═╦═╦╦╦══╦╗╔╦═╦╗╔╦═╦══╦═╗')
+    print('       ║╔╣║║╔╩╗╔╣╚╝║╔╣╚╝║╦╬╗╚╣╦╣')
+    print('       ╚╝╚═╩╝ ╚╝╚══╩═╩══╩═╩══╩═╝')
+    
+    print('\t   ╔═╦═╦╦═╦╗╔╦══╦╦╗')
+    print('\t   ║╦╣║║║╔╣╚╣╠╗╚╣═║')
+    print('\t   ╚═╩╩═╩═╩═╩╩══╩╩╝')
+    
+    print('\t      ╔═╦═╦═╦╦╗')
+    print('\t      ║╣╣╩║╠╣═╣')
+    print('\t      ╚═╩╩╩═╩╩╝') 
+    
+#opções para seu avatar inicial
+avatar_inicial0 = ('( • ‿ • )')
+avatar_inicial1 = ('( •  ͟ʖ • )')
+avatar_inicial2 = ('( • ₃ • )')
+
+#skins de avatares da loja
+
+
+avatar0 = '⎛⎝(•ⱅ•)⎠⎞'
+avatar1 = 'ʕ ·(エ)· ʔ'
+avatar2 = '（ ´_⊃｀）'
+avatar3 = '{•̃_•̃}'
+avatar4 = '[¬º-°]¬'
+avatar5 = '( ˇ෴ˇ )'
+avatar6 = '⊂(◉‿◉)つ'
+avatar7 = 'ᕦ(ò_óˇ)ᕤ'
+avatar8 = '(❍ᴥ❍ʋ)'
+avatar9 = '༼ つ ╹ ╹ ༽つ'
+avatar10 = '=^._.^= ∫'
+avatar11 = 'υ´• ﻌ •`υ'
+avatar12 = 'ʕ( ͡❛ ͜ʖ ͡❛)ʔ'
+avatar13 = '(\_/)\n\t             ( •,•)\n\t             (")_(")'
+
+#preco dos avatares
+preco_avatar = 0
+
+def avatares_iniciais ():  #mostra as imagens dos avatares iniciais
+    print('0:', avatar_inicial0)
+    print('1:', avatar_inicial1)
+    print('2:', avatar_inicial2)
+
+def avatares_loja ():  #mostra as imagens dos avatares compráveis
+    print('\n\nMorceguinho:', avatar0, '\nValor:', preco_avatar)
+    print('Urso Inconformado:', avatar1, '\nValor:', preco_avatar)
+    print('Doug:', avatar2, '\nValor:', preco_avatar)
+    print('Robô:', avatar3, '\nValor:', preco_avatar)
+    print('Zombie:', avatar4, '\nValor:', preco_avatar)
+    print('Bigode mais Estiloso do Mundo:', avatar5, '\nValor:', preco_avatar)
+    print('Kirby Atropelado:', avatar6, '\nValor:', preco_avatar)
+    print('Rato de Academia:', avatar7, '\nValor:', preco_avatar)
+    print('Jake:', avatar8, '\nValor:', preco_avatar)
+    print('Boo:', avatar9, '\nValor:', preco_avatar)
+    print('Gatinho Furioso:', avatar10, '\nValor:', preco_avatar)
+    print('Cachorrinho:', avatar11, '\nValor:', preco_avatar)
+    print('Homem Pão de Forma Sedutor:', avatar12, '\nValor:', preco_avatar)
+    print('Coelho Maneiríssimo:', avatar13, '\nValor:', preco_avatar)
+    
+moedas = 0 #dinheiro no jogo do usuário
+
+def loja1 (): #se o usuário quiser comprar algo na loja, menu loja em português
+    print('╔══════════════╗')
+    print('  MOEDAS:', moedas)
+    print('╚══════════════╝')
+    
+    print('\t ╔╦╦╦══╦═╦═╦╗')
+    print('\t ║║║║╔╗╠╗║║║║')
+    print('\t ║║╚╣╚╝╠╣║╩║║')
+    print('\t ║╚═╩══╩═╩╩╝║')
+    print('\t ╚══════════╝')
+    
+    print('\t ╔══╦╦╦╦═╦╦══╗')
+    print('\t ╠╗╚╣═╣║║║╠╗╚╣')
+    print('\t ╚══╩╩╩╩╩═╩══╝')
+    
+    print('╔═╦╗╔═╦═╦╦═╗╔═╦═╗╔═╦╗╔╦═╦╦═╦═╗')
+    print('║╔╣╚╣╩║║║║║║║╠║╦╣║╔╣╚╝║║║║╠║║║')
+    print('╚╝╚═╩╩╩╩═╩═╝╚═╩═╝╚╝╚══╩╩═╩═╩═╝')
+    
+    print('    ╔═╦═╦═╦══╦══╦═╦╦╦╦═╦══╗')
+    print('    ║╩║╠╣╦╬╗╚╬╗╚╣║║╔╣║║╠╗╚╣')
+    print('    ╚╩╩═╩═╩══╩══╩═╩╝╚╩═╩══╝')
+    
+    print('\t ╔══╦═╦═╦╦╗╔╗')
+    print('\t ║║║║╦╣║║║╚╝║')
+    print('\t ╚╩╩╩═╩╩═╩══╝')
+    
+def loja2 (): #se o usuário quiser comprar algo na loja, menu loja em inglês
+    print('╔══════════════╗')
+    print('  COINS:', moedas)
+    print('╚══════════════╝')
+    
+    print('\t╔╦═╦╗╔╦══╦═╦╗')
+    print('\t║║╚╣╚╝║╔╗║░║║')
+    print('\t║╠╗║╔╗║╚╝║╔╝║')
+    print('\t║╚═╩╝╚╩══╩╝ ║')
+    print('\t╚═══════════╝')
+    
+    print('\t╔══╦╦╦╦═╦╦══╗')
+    print('\t╠╗╚╣═╣║║║╠╗╚╣')
+    print('\t╚══╩╩╩╩╩═╩══╝')
+    
+    print('     ╔╦╦╦═╦╗╔╗╔═╦═╦═╦═╦╦╗')
+    print('     ║║║║╩║╚╣╚╣╔╣╩║╔╣╦╣╔╝')
+    print('     ╚══╩╩╩═╩═╩╝╚╩╩╝╚═╩╝')
+    
+    print('  ╔═╦═╦═╦═╦══╦══╦═╦╦╦╦═╦══╗')
+    print('  ║╩║╠╣╠╣╦╬╗╚╬╗╚╣║║╔╣║╦╬╗╚╣')
+    print('  ╚╩╩═╩═╩═╩══╩══╩═╩╝╚╩═╩══╝')
+    
+    print('\t ╔══╦═╦═╦╦╗╔╗')
+    print('\t ║║║║╦╣║║║╚╝║')
+    print('\t ╚╩╩╩═╩╩═╩══╝')
+
+#planos de fundo 
+wallpaper1 = '______\n|    |\n|   \n|  \n|_'
+wallpaper2 = '༼つಠ益ಠ༽つ ─=≡ΣO))'
+wallpaper3 = '( -_•)╦̵̵╤─'
+
+#preços dos planos de fundo
+preco_wallpaper = 0
+
+def wallpapers_loja ():
+    print('\nLaser Tag:', wallpaper2, '\nValor:', preco_wallpaper)
+    print('Tiro ao alvo:', wallpaper3, '\nValor:', preco_wallpaper)
+    
+def skins1 (): #se o usuário quiser comprar alguma skin, skins em português
+    print('╔══════════════╗')
+    print('  MOEDAS:', moedas)
+    print('╚══════════════╝')
+    
+    print('\t╔╦═╦╦═╦╦═╦╦═╦╗')
+    print('\t║║╚╣╝╔╣║║║║╚╣║')
+    print('\t║╠╗║╗╚╣║║║╠╗║║')
+    print('\t║╚═╩╩═╩╩╩═╩═╝║')
+    print('\t╚════════════╝')
+
+    avatares_loja()
+    
+    print('\t╔╦╦═╦╗╔══╦═╦╦╗')
+    print('\t║║║║║╚╬╗╔╣╩║╔╝')
+    print('\t╚═╩═╩═╝╚╝╚╩╩╝')
+    
+def skins2 (): #se o usuário quiser comprar alguma skin, skins em inglês
+    print('╔══════════════╗')
+    print('  COINS:', moedas)
+    print('╚══════════════╝')
+    
+    print('\t╔╦═╦╦═╦╦═╦╦═╦╗')
+    print('\t║║╚╣╝╔╣║║║║╚╣║')
+    print('\t║╠╗║╗╚╣║║║╠╗║║')
+    print('\t║╚═╩╩═╩╩╩═╩═╝║')
+    print('\t╚════════════╝')  
+    
+    avatares_loja()
+
+    print('\t  ╔═╦═╦═╦╦╗')
+    print('\t  ║╣╣╩║╠╣═╣')
+    print('\t  ╚═╩╩╩═╩╩╝')
+    
+def wallpaper_loja1 (): #se o usuário quiser comprar algum wallpaper, wallpaper em português
+    print('╔══════════════╗')
+    print('  MOEDAS:', moedas)
+    print('╚══════════════╝')
+    
+    print('\t╔╦═╦╦╦═╦═╦╦══╦╦═╦╦═╦╦═╦╗╔╦═╦╦═╦╦══╦╗')
+    print('\t║║░║║║║║║║║╔╗║║║╚╣═╣║═╣║║║║║║║╚╣╔╗║║')
+    print('\t║║╔╣╚╣╩║║║║╚╝║║║╔╣═╣║╔╣╚╝║║║║║╔╣╚╝║║')
+    print('\t║╚╝╚═╩╩╩╩═╩══╝╚═╝╚═╝╚╝╚══╩╩═╩═╝╚══╝║')
+    print('\t╚══════════════════════════════════╝')  
+
+    wallpapers_loja()
+    
+    print('\t\t  ╔╦╦═╦╗╔══╦═╦╦╗')
+    print('\t\t  ║║║║║╚╬╗╔╣╩║╔╝')
+    print('\t\t  ╚═╩═╩═╝╚╝╚╩╩╝')
+    
+def wallpaper_loja2 (): #se o usuário quiser comprar algum wallpaper, wallpaper em inglês
+    print('╔══════════════╗')
+    print('  COINS:', moedas)
+    print('╚══════════════╝')
+    
+    print('\t╔╦╦╦╦═╦╦╦╦╦═╦═╦═╦═╦══╦╗')
+    print('\t║║║║║║║║║║║░║║║░║═╣░░║║')
+    print('\t║║║║║╩║╚╣╚╣╔╣╩║╔╣═╣╔╗╣║')
+    print('\t║╚══╩╩╩═╩═╩╝╚╩╩╝╚═╩╝╚╝║')
+    print('\t╚═════════════════════╝')
+
+    wallpapers_loja()
+    
+    print('\t       ╔═╦═╦═╦╦╗')
+    print('\t       ║╣╣╩║╠╣═╣')
+    print('\t       ╚═╩╩╩═╩╩╝')
+
+#se o jogador ganhar a partida
+def ganhou_facil1 (): #ganhou, modo: fácil, português
+    print('\n{\__/}      PARABÉNS,')
+    print('( ●_●)      VOCÊ GANHOU CINCO')
+    print('(  >[̲̅$̲̅(̲̅5̲̅)̲̅$̲̅] MOEDAS!')
+
+def ganhou_medio1 (): #ganhou, modo: médio, português
+    print('\n{\__/}       PARABÉNS,')
+    print('( ●_●)       VOCÊ GANHOU DEZ')
+    print('(  >[̲̅$̲̅(̲̅1̲̅0̲̅)̲̅$̲̅] MOEDAS!')
+    
+def ganhou_dificil1 (): #ganhou, modo: difícil, português
+    print('\n{\__/}        PARABÉNS,')
+    print('( ●_●)        VOCÊ GANHOU CEM')
+    print('(  >[̲̅$̲̅(̲̅ιοο̲̅)̲̅$̲̅] MOEDAS!')
+
+def ganhou_facil2 (): #ganhou, modo: fácil, inglês
+    print('\n{\__/}      CONGRATULATIONS,')
+    print('( ●_●)      YOU WON FIVE')
+    print('(  >[̲̅$̲̅(̲̅5̲̅)̲̅$̲̅] COINS!')
+
+def ganhou_medio2 (): #ganhou, modo: médio, inglês
+    print('\n{\__/}       CONGRATULATIONS,')
+    print('( ●_●)       YOU WON TEN')
+    print('(  >[̲̅$̲̅(̲̅1̲̅0̲̅)̲̅$̲̅] COINS!')
+    
+def ganhou_dificil2 (): #ganhou, modo: difícil, inglês
+    print('\n{\__/}        CONGRATULATIONS,')
+    print('( ●_●)        YOU WON A HUNDRED')
+    print('(  >[̲̅$̲̅(̲̅ιοο̲̅)̲̅$̲̅] COINS!')
+
+#se o jogador perder a partida   
+def perdeu ():
+    print('|￣￣￣￣￣￣￣|')
+    print('|   Perdeu!   |')
+    print('|＿＿＿ _＿＿＿|')
+    print('(\__/)|| ')
+    print('(•ㅅ•)|| ')
+    print('/ 　 づ')
+    
+entrar_jogo = input("Deseja abrir o jogo? ('s' ou 'n')\n ") #iniciar do jogo
+
+if entrar_jogo != 's' and entrar_jogo != 'n': #mensagem de erro para controle
+    while entrar_jogo != 's' and entrar_jogo != 'n':
+        print('Resposta inválida. Digite novamente!')
+        entrar_jogo = input("Deseja abrir o jogo? ('s' ou 'n')\n ")
+        
+if entrar_jogo == 's': #caso o usuário escolha iniciar o jogo (ele estará pré-configurado em português)
+    funcao = 'menu'
+    
+    if funcao == 'menu':
+        while funcao == 'menu':
+            menuiniciar1 ()
+    
+            funcao = input('O que deseja fazer?\n ')
+    
+            if funcao != 'jogar' and funcao != 'opcoes' and funcao != 'loja' and funcao != 'sair': #mensagem de erro para controle
+                while funcao != 'jogar' and funcao != 'opcoes' and funcao != 'loja' and funcao != 'sair':
+                    print('Resposta inválida. Digite novamente!')
+                    funcao = input('O que deseja fazer?\n ')
+    
+            if funcao == 'opcoes':
+                    funcao = 'voltar'
+            
+                    while funcao == 'voltar':
+                        opcoes1()
+            
+                        funcao = input('O que deseja fazer?\n ')
+                        if funcao != 'regras' and funcao != 'dificuldade' and funcao != 'idioma' and funcao != 'menu':
+                            while funcao != 'regras' and funcao != 'dificuldade' and funcao != 'idioma' and funcao != 'menu':
+                                print('Resposta inválida. Digite novamente!')
+                                funcao = input('O que deseja fazer?\n ')
+                
+                        if funcao == 'regras':
+                            regras1()
+                
+                            funcao = input('O que deseja fazer?\n ')
+                            if funcao != 'voltar':
+                                while funcao != 'voltar':
+                                    print('Resposta inválida. Digite novamente!')
+                                    funcao = input('O que deseja fazer?\n ')                
+                
+                        if funcao == 'dificuldade':
+                            dificuldade1()
+                
+                            funcao = input('O que deseja fazer?\n ')
+                            if funcao != 'facil' and funcao != 'medio' and funcao != 'dificil' and funcao != 'voltar':
+                                while funcao != 'facil' and funcao != 'medio' and funcao != 'dificil' and funcao != 'voltar':
+                                    print('Resposta inválida. Digite novamente!')
+                                    funcao = input('O que deseja fazer?\n ')
+                
+                        if funcao == 'idioma':
+                            idioma1()
+                
+                            funcao = input('O que deseja fazer?\n ')
+                            if funcao != 'portugues' and funcao != 'ingles' and funcao != 'voltar':
+                                while funcao != 'portugues' and funcao != 'ingles' and funcao != 'voltar':
+                                    print('Resposta inválida. Digite novamente!')
+                                    funcao = input('O que deseja fazer?\n ')
+            if funcao == 'loja':
+                funcao = 'voltar'
+                while funcao == 'voltar':
+                    loja1()
+                    
+                    funcao = input('O que deseja fazer?\n ')
+                    if funcao != 'skins' and funcao != 'plano de fundo' and funcao != 'acessorios' and funcao != 'menu':
+                        while funcao != 'skins' and funcao != 'plano de fundo' and funcao != 'acessorios' and funcao != 'menu':
+                            print('Resposta inválida. Digite novamente!')
+                            funcao = input('O que deseja fazer?\n ')
+                    
+                    if funcao == 'skins':
+                        skins1()
+                        
+                        funcao = input('O que deseja fazer?\n ')
+                        if funcao != 'voltar':
+                            while funcao != 'voltar':
+                                print('Resposta inválida. Digite novamente!')
+                                funcao = input('O que deseja fazer?\n ')
+                                
+                    if funcao == 'plano de fundo':
+                        wallpaper_loja1()
+                        
+                        funcao = input('O que deseja fazer?\n ')
+                        if funcao != 'voltar':
+                            while funcao != 'voltar':
+                                print('Resposta inválida. Digite novamente!')
+                                funcao = input('O que deseja fazer?\n ')
+                                
+                    if funcao == 'acessorios':
+                        #inacabado
+                        
+                        funcao = input('O que deseja fazer?\n ')
+                        if funcao != 'voltar':
+                            while funcao != 'voltar':
+                                print('Resposta inválida. Digite novamente!')
+                                funcao = input('O que deseja fazer?\n ')
+                    
+            if funcao == 'sair':
+                print('(ಥ﹏ಥ)')
+            
+else: #caso o usuário não queira iniciar o jogo =(
+    print('(ಥ﹏ಥ)')
+
+
+# In[ ]:
+
+
+
+
